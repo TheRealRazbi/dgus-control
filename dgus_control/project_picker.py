67bdb087af02dc5c90d06f14607c81b8b9a8f83f
Razbi <38946621+TheRealRazbi@users.noreply.github.com>
2025-06-10 01:51:18 +0300

    feat(porting): add functional template picker

--- dgus_control/project_picker.py
+++ dgus_control/project_picker.py
@@ -6,9 +6,13 @@
 
 __all__ = ['DGUSPicker', 'ManualPicker', 'TemplatePicker', 'OCRPicker']
 
+from dataclasses import dataclass
 from typing import Protocol
 
-from stageclick.core import Window
+from numpy import ndarray
+from stageclick.core import Window, Button
+
+from dgus_control.log import log_colored
 
 
 class DGUSPicker(Protocol):
@@ -21,9 +25,26 @@ class ManualPicker:
         raise NotImplementedError
 
 
+@dataclass
+class TemplateProject:
+    select_path_template: ndarray
+    top_path_template: ndarray
+
+
+@dataclass
 class TemplatePicker:
-    def pick(self, *args, **kwargs):
-        return NotImplementedError
+    options: dict[str, TemplateProject]
+    click_offset: tuple[int, int] = 80, 15
+    wait_until_ready: bool = True
+    timeout_ready: float = 5.0
+
+    def pick(self, window: Window, project_name: str):
+        project = self.options[project_name]
+        Button(window, project.select_path_template, click_offset=self.click_offset).click()
+        log_colored(f"Picking project {project_name}...", "yellow")
+        if self.wait_until_ready:
+            window.wait_for_template(project.top_path_template, timeout=self.timeout_ready)
+            log_colored(f"Project {project_name} picked successfully.", "green")
 
 
 class OCRPicker:
